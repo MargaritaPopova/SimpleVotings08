ea6642fae72e98f666c4cebfc6e9d60726119bfa
Margarita Popova <margarita.kistanova@gmail.com>
2020-12-05 13:00:54 +0300

    Update tests.py

--- main_app/tests.py
+++ main_app/tests.py
@@ -5,7 +5,6 @@ class TestView(TestCase):
   
     def setUp(self):
       self.client = Client()
-      self.user = User.objects.create_user(username='testuser', password='12345678qwerty')
       self.response = self.client.get('/')
       
     def test_menu(self):
