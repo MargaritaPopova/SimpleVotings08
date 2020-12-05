583a0dfbd9b3de6cdca6134d27cd59fa2df6bb1e
Margarita Popova <margarita.kistanova@gmail.com>
2020-12-05 12:58:11 +0300

    Update tests.py

--- main_app/tests.py
+++ main_app/tests.py
@@ -1,1 +1,13 @@
-# Create your tests here.
+from django.test import TestCase
+from django.test import Client
+
+class TestView(TestCase):
+  
+    def setUp(self):
+      self.client = Client()
+      self.user = User.objects.create_user(username='testuser', password='12345678qwerty')
+      self.response = self.client.get('/')
+      
+    def test_menu(self):
+      self.assertContains(self.response, 'Login')
+    
